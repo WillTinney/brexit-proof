a245662ca2cdae9b1eaba52e00e2fe2a613776ad
Will Tinney <willtinney1@gmail.com>
2017-02-23 17:25:51 +0000

    User spec

--- spec/features/edit_user_spec.rb
+++ spec/features/edit_user_spec.rb
@@ -5,11 +5,15 @@ feature 'User Edit Page', js: false do
 
   before do
     login_as(user, :scope => :user)
-    visit "/users/#{user.id}/edit"
+    visit "/users/#{user.id}"
   end
 
   it 'can be reached' do
     expect(page.status_code).to eq(200)
+  end
+
+  it 'has a profile picture upload field' do
+    expect(page).to have_field('user_profile_picture', type: 'file')
   end
 
   it 'has an e-mail field' do
@@ -24,12 +28,29 @@ feature 'User Edit Page', js: false do
     expect(page).to have_field('user_first_name', type: 'text')
   end
 
+  it 'has a middle name field' do
+    expect(page).to have_field('user_middle_name', type: 'text')
+  end
+
   it 'has a last name field' do
     expect(page).to have_field('user_last_name', type: 'text')
   end
 
+  it 'has a citizenship field' do
+    expect(page).to have_field('user_citizenship', type: 'text')
+  end
+
+  it 'has a date of birth field' do
+    expect(page).to have_field('user_date_of_birth', type: 'text')
+  end
+
+  it 'has a phone number field' do
+    expect(page).to have_field('user_phone_number', type: 'text')
+  end
+
   it 'has an address field' do
-    expect(page).to have_field('user_address', type: 'text')
+    expect(page).to have_field('user_address_line_1', type: 'text')
+    expect(page).to have_field('user_address_line_2', type: 'text')
   end
 
   it 'has a city field' do
@@ -40,54 +61,36 @@ feature 'User Edit Page', js: false do
     expect(page).to have_field('user_postcode', type: 'text')
   end
 
-  it 'has an employment field' do
-    expect(page).to have_field('user_employment', type: 'text')
-  end
-
-  it 'has a date of birth field' do
-    expect(page).to have_field('user_date_of_birth', type: 'text')
-  end
-
-  it 'has a photo ID upload field' do
-    expect(page).to have_field('user_photo_id', type: 'file')
-  end
-
-  scenario 'user who fills in and submits all their details is eligible to apply for a loan' do
+  scenario 'user can fill in all their details' do
     user_fills_in_details
-    user_uploads_photo_id
     click_on 'Save'
+    # expect(page).to be('users/user_id/profile')
     expect(page).to have_content('Ask for a loan')
   end
 
-  scenario 'user uploads a bad photo should fail face recognition' do
-    user_fills_in_details
-    user_uploads_bad_photo
+  scenario 'user can upload a profile picture' do
+    user_uploads_profile_picture
     click_on 'Save'
-    expect(page).to have_content('Face recognition failed')
+    # expect(page).to be('users/user_id/profile')
+    expect(page).to have_content('Ask for a loan')
   end
 
-  scenario 'form is re-rendered if a required field is left blank (e.g. DoB)' do
-    user_forgets_date_of_birth
-    click_on 'Save'
-    expect(page).to have_content('Please enter your date of birth')
-  end
+  # scenario 'user wants to change his password' do
+  #   visit "/users/edit.#{user.id}"
+  #   expect(page).to have_content('Edit User')
+  #   fill_in 'user_current_password', with: 'password'
+  #   fill_in 'user_password', with: 'new_password'
+  #   fill_in 'user_password_confirmation', with: 'new_passwords'
+  #   click_on 'Update'
 
-  scenario 'user wants to change his password' do
-    visit "/users/edit.#{user.id}"
-    expect(page).to have_content('Edit User')
-    fill_in 'user_current_password', with: 'password'
-    fill_in 'user_password', with: 'new_password'
-    fill_in 'user_password_confirmation', with: 'new_passwords'
-    click_on 'Update'
+  #   expect(page).to have_content("doesn't match Password")
+  #   fill_in 'user_current_password', with: 'password'
+  #   fill_in 'user_password', with: 'new_password'
+  #   fill_in 'user_password_confirmation', with: 'new_password'
+  #   click_on 'Update'
 
-    expect(page).to have_content("doesn't match Password")
-    fill_in 'user_current_password', with: 'password'
-    fill_in 'user_password', with: 'new_password'
-    fill_in 'user_password_confirmation', with: 'new_password'
-    click_on 'Update'
-
-    expect(current_path).to eq(user_path(user))
-  end
+  #   expect(current_path).to eq(user_path(user))
+  # end
 
   private
 
@@ -95,28 +98,18 @@ feature 'User Edit Page', js: false do
     choose('Mr')
     fill_in 'user_first_name', with: 'John'
     fill_in 'user_last_name', with: 'Smith'
-    fill_in 'user_address', with: 'New Street'
-    fill_in 'user_city', with: 'Johannesburg'
-    fill_in 'user_postcode', with: '23142'
-    fill_in 'user_employment', with: 'banker'
+    fill_in 'user_citizenship', with: 'French'
+    fill_in 'user_date_of_birth', with: '31/07/1980'
+    fill_in 'user_phone_number', with: '07123 123123'
+    fill_in 'user_address_line_1', with: '12 Example Road'
+    fill_in 'user_address_line_2', with: 'New Example'
+    fill_in 'user_city', with: 'Exampleville'
+    fill_in 'user_postcode', with: 'EX14 MPL'
     fill_in 'user_date_of_birth', with: '31/07/1980'
   end
 
-  def user_uploads_photo_id
-    attach_file 'user_photo_id', 'spec/files/testpassport.jpg'
-  end
-
-  def user_uploads_bad_photo
-    attach_file 'user_photo_id', 'spec/files/badphoto.jpg'
-  end
-
-  def user_forgets_date_of_birth
-    choose('Mr')
-    fill_in 'user_first_name', with: 'John'
-    fill_in 'user_last_name', with: 'Smith'
-    fill_in 'user_address', with: 'New Street'
-    fill_in 'user_city', with: 'Johannesburg'
-    fill_in 'user_postcode', with: '23142'
-    fill_in 'user_employment', with: 'banker'
+  def user_uploads_profile_picture
+    click_on 'Change Picture'
+    attach_file 'user_profile_picture', 'spec/files/testpassport.jpg'
   end
 end
